b268b6384e1c3c36ac979e9f8d0d3f302044f567
Academy_ekat@itstep.academy <Academy_ekat@itstep.academy>
2022-09-02 21:10:01 +0500

    Добавьте файлы проекта.

---
+++ CSharp(02.09.2022)/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_02._09._2022_ {
+    class Program {
+        static void PrintRed(string text) {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(text);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+
+        static void PrintBlue(string text) {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(text);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        static void PrintGreen(string text) {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(text);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        static void MainMethod(int zzz, Action<string> delPrint) {
+            zzz *= zzz;
+            delPrint(zzz.ToString());
+        }
+        static void Main(string[] args) {
+            MainMethod(2, PrintRed);
+            MainMethod(2, PrintBlue);
+            MainMethod(2, PrintGreen);
+
+            SuperClass<int, int> a = new SuperClass<int, int>();
+            SuperClass<string, List<int>> b = new SuperClass<string, List<int>>();
+
+            Console.WriteLine();
+        }
+    } 
+}
---
+++ CSharp(02.09.2022)/Делегаты.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_02._09._2022_ {
+
+    class SuperClass<NeZnayuTipDannih, NeZnayuTipDannih1> {
+        public int MyProperty { get; set; }
+        public int MyProperty1 { get; set; }
+        public NeZnayuTipDannih MyProperty2 { get; set; }
+        public NeZnayuTipDannih1 MyProperty3 { get; set; }
+
+       
+    }
+}
